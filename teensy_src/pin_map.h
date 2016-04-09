221278ec29ec0a09e5038850645a66692da01932
Jordan Less'ard-Springett <jordan80100@gmail.com>
2016-04-09 18:10:49 +0100

    Removed old pin defines

--- teensy_src/pin_map.h
+++ teensy_src/pin_map.h
@@ -27,49 +27,6 @@ extern "C" {
 #define RX3		7
 #define TX3		8
 
-#define A0		PORT_A, 0
-#define A1		PORT_A, 1
-#define A2		PORT_A, 2
-#define A3		PORT_A, 3
-#define A4		PORT_A, 4
-#define A5		PORT_A, 5
-#define A12		PORT_A, 12
-#define A13		PORT_A, 13
-
-#define B0		PORT_B, 0
-#define B1		PORT_B, 1
-#define B2		PORT_B, 2
-#define B3		PORT_B, 3
-#define B16		PORT_B, 16
-#define B17		PORT_B, 17
-#define B18		PORT_B, 18
-#define B19		PORT_B, 19
-
-#define C0		PORT_C, 0
-#define C1		PORT_C, 1
-#define C2		PORT_C, 2
-#define C3		PORT_C, 3
-#define C4		PORT_C, 4
-#define C5		PORT_C, 5
-#define C6		PORT_C, 6
-#define C7		PORT_C, 7
-#define C8		PORT_C, 8
-#define C9		PORT_C, 9
-#define C10		PORT_C, 10
-#define C11		PORT_C, 11
-
-#define D0		PORT_D, 0
-#define D1		PORT_D, 1
-#define D2		PORT_D, 2
-#define D3		PORT_D, 3
-#define D4		PORT_D, 4
-#define D5		PORT_D, 5
-#define D6		PORT_D, 6
-#define D7		PORT_D, 7
-
-#define E0		PORT_E, 0
-#define E1		PORT_E, 1
-
 #endif
 
 #ifdef __cplusplus
