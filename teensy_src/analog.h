6d18776f8c005767faf0c3ff36167ce058b40da1
Jordan Less'ard-Springett <jordan80100@gmail.com>
2016-04-30 15:06:13 +0100

    Added new IO file and analog, not complete yet

---
+++ teensy_src/analog.h
@@ -0,0 +1,28 @@
+/****************************************************************************************
+ *
+ * File:
+ * 		analog.h
+ *
+ * Purpose:
+ * 		Provides functions for interacting with the teensy analog pins.
+ *
+ *
+ ***************************************************************************************/
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifndef TEENSY_IO_H
+#define TEENSY_IO_H
+
+
+void adc_init();
+
+
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
